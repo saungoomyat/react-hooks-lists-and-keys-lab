9f06d983ef06836cf1f45ec8856a46974bc9520a
Saung Oo Myat <saung.myat@gmail.com>
2022-04-17 21:59:51 -0400

    commit tree 6add25f3e6186d2e86e74e7dabcbf6332e40d197

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,7 +3,13 @@ import React from "react";
 function NavBar() {
   const links = ["home", "about", "projects"];
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  const page = links.map((link) => (
+    <a key={link} href={"#" + link}>
+      {link}
+    </a>
+  ));
+
+  return <nav>{page}</nav>;
 }
 
 export default NavBar;
